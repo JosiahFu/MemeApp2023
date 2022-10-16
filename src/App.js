709d82fcc2711a26fa85b509187e85714ffa3a1f
Gabriel Chavez <specter.assassin.e2o4@gmail.com>
2022-10-16 16:13:50 -0700

    This code was written by a group of 6 people at the same time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="flip">
+      <h1>WRITE STUFF BELOW HERE vvvv</h1>
+      <img src="https://i0.wp.com/among-us.me/wp-content/uploads/2022/09/Among-Us-Red-Costume.jpg?fit=1000%2C1000&ssl=1" className="amogus" />
+      <img src="https://i.pinimg.com/736x/52/ac/99/52ac99c15349b5d678001f83e4ac283e--laughing-so-hard-cant-stop-laughing.jpg" className="Drake " />
+      <h1>"This is gonna give me a mental breakdown" -Aiden 2022</h1>
+      <h3>ALL AROUND ME ARE FAMILIAR FACESSSSSSs</h3>
+      <h2> this is pain and suffering</h2>
+      <ol>
+        <li>:3</li>
+        <li>are you sure about that</li>
+      </ol>
+      <p>apple sus apple</p>
+      <p>AMOUNG US! EVERYONE IS SUS!!!!!!</p>
+      <ol></ol>
+      <ul>What is your favorite sport?</ul>
+      <p>Robotics</p>
+      <h1> same</h1>
+      <ul> favorite coding language?</ul>
+      <ul>favorite meal?</ul>
+      <p>Robotics</p>
+      <ul>Favorite school subject?</ul>
+      <p>Robotics</p>
+      <p>How can you eat robotics?</p>
+      <ul>Dominos or Pizza Hut?</ul>
+      <p>Robotics</p>
+      <ul> Favorite movie?</ul>
+      <p>Robotics</p>
+      <h1>"Good cherries taste good" - braden 2022</h1>
+      <h1>Cherries are bad!</h1>
+      <h1>Veggies are better than fruits!!  </h1>
+      <h1> Robotics is the best! Join the period 2 class! </h1>
+      <h1>Math is red! </h1>
+      <h1>Spanish is DARK RED </h1>
+      <h1> Science is pink </h1>
+      <img src="https://m.media-amazon.com/images/I/815ckfBJW7L.jpg" width="200" />
+
+      <h1>This is gunna give me a mental breakdown -Aiden 2022</h1>
+      <p>HELLO HOW DO I MAKE THIS BOLD</p>
+      <p> What college do you want to go to Aiden?</p>
+
+      <h1> Please stop deleting everything!!!!!! </h1>
+      <p> CODE CORRECTLY Please!!!!!!</p>
+      <p>have GP yall!!!!</p>
     </div>
   );
+
 }
 
 export default App;
